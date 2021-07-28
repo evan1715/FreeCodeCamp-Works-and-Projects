e0e39269c578f2c2c119b7a666f9a079d0b490c3
Evan <evan1715@users.noreply.github.com>
2021-07-28 11:29:02 -0400

    spinal tap case

--- JS Algs and Data Structures/intermediate-algs.js
+++ JS Algs and Data Structures/intermediate-algs.js
@@ -11,3 +11,23 @@ function whatIsInAName(collection, source) {
     return arr;
 }
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+//Spinal Tap Case
+function spinalCase(str) {
+    //To search or extract a pattern more than once, you can use the g flag.
+    //To ignore letter casing, use the i flag.
+    // let cake = str.toLowerCase();
+    // (/\s/g).test(cake); //returns true or false
+    // cake.match(/\s/g);
+
+    //icing will place spaces before words with a capital letter.
+    //The () create capturing groups and we use $1 $2 to access them.
+    let icing = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+    str = icing.replace((/\s|_/g), '-').toLowerCase();
+
+    return str;
+}
+spinalCase('This Is Spinal Tap');
+
+
