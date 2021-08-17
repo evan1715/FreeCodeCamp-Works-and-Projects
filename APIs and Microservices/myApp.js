e63ba69d84a8ae8ab09423e5fade60ac6057dafb
Evan <evan1715@users.noreply.github.com>
2021-08-17 15:25:15 -0400

    fcc progress

--- APIs and Microservices/myApp.js
+++ APIs and Microservices/myApp.js
@@ -3,9 +3,23 @@ const app = express();
 
 /* In Express, routes takes the following structure: app.METHOD(PATH, HANDLER). METHOD is an http method in lowercase. PATH is a relative path on the server (it can be a string, or even a regular expression). HANDLER is a function that Express calls when the route is matched. Handlers take the form function(req, res) {...}, where req is the request object, and res is the response object. */
 
-app.get('/', (req, res) => {
-    res.send('Hello Express');
+const logger = (req, res, next) => {
+    console.log(req.method, req.path +" - " +req.ip);
+    next();
+}
+
+app.get('/', logger, (req, res) => {
+    // res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html')
 });
+
+app.get('/json', logger, (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
+})
 
 
 module.exports = app;
